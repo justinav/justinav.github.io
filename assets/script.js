72685e419fa8fca8a7892d8f850032d0f1fe8e89
justinav <justinavil@gmail.com>
2016-04-03 21:34:51 -0400

    site redesign

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+d3.json("assets/projects.json", function(data) {
+
+  var projectContainer = d3.select(".grid").selectAll(".grid-item")
+  .data(data)
+  .enter()
+  .append("article")
+  .classed("grid-item", true);
+
+  var imgLink = projectContainer.append("a")
+  .attr("href", function(d) {
+    if (d.url.length > 1) {
+      return d.url;
+    }
+  });
+
+  imgLink.append("img")
+  .classed("photoContainer", true)
+  .attr("src", function(d) {
+    return "assets/images/" + d.src
+  })
+
+  var caption = projectContainer.append("ul")
+  .classed("caption", true);
+
+  var titleLink = caption.append("a")
+    .attr("href", function(d) {
+      if (d.url.length > 1) {return d.url;}
+    });
+
+  titleLink.append("li")
+  .classed("captionHeadline", true)
+  .text(function(d) {
+    return d.title;
+  });
+
+  caption.append("li")
+  .text(function(d) {
+    return d.description;
+  })
+
+  caption.append("li").append("a")
+  .attr("href", function(d) {
+    if (d.downloadUrl.length > 1) {
+      return d.downloadUrl;
+    } else if (d.githubUrl.length > 1) {
+      return d.githubUrl;
+    } else if (d.url.length > 1 ) {
+      return d.url;
+    }else {
+      this.remove();
+    }
+  })
+  .classed("btn", true)
+  .text(function(d) {
+    if (d.downloadUrl.length > 1) {
+      return "download"
+    } else if (d.githubUrl.length > 1) {
+      return "github"
+    } else if (d.url.length > 1) {
+      return "visit"
+    }
+  });
+});
