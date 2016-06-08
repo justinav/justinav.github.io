ddfc6e7261df77bcfb2585d59520b6b77d8a1510
justinav <justinavil@gmail.com>
2016-06-08 14:38:40 -0400

    cleaning up old d3 code

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,7 @@
-// d3.json("assets/projects.json", function(data) {
-
-//   var projectContainer = d3.select(".grid").selectAll(".grid-item")
-//   .data(data)
-//   .enter()
-//   .append("article")
-//   .classed("grid-item", true);
-
-//   var imgLink = projectContainer.append("a")
-//   .attr("href", function(d) {
-//     if (d.url.length > 1) {
-//       return d.url;
-//     }
-//   });
-
-//   imgLink.append("img")
-//   .classed("photoContainer", true)
-//   .attr("src", function(d) {
-//     return "assets/images/" + d.src
-//   })
-
-//   var caption = projectContainer.append("ul")
-//   .classed("caption", true);
-
-//   caption.append("li").append("a")
-//   .classed("captionHeadline", true)
-//   .attr("href", function(d) {
-//     if (d.url.length > 1) {return d.url;}
-//   })
-//   .text(function(d) {
-//     return d.title;
-//   });
-
-//   caption.append("li")
-//   .text(function(d) {
-//     return d.description;
-//   })
-
-//   caption.append("li").append("a")
-//   .attr("href", function(d) {
-//     if (d.downloadUrl.length > 1) {
-//       return d.downloadUrl;
-//     } else if (d.githubUrl.length > 1) {
-//       return d.githubUrl;
-//     } else if (d.url.length > 1 ) {
-//       return d.url;
-//     }else {
-//       this.remove();
-//     }
-//   })
-//   .classed("btn", true)
-//   .text(function(d) {
-//     if (d.downloadUrl.length > 1) {
-//       return "download"
-//     } else if (d.githubUrl.length > 1) {
-//       return "github"
-//     } else if (d.url.length > 1) {
-//       return "visit"
-//     }
-//   });
-
-//   projectContainer.selectAll("a")
-//     .attr("target", "_blank");
-
-//   d3.select(".logo")
-//     .on("click", function(){d3.select(this).classed("logoMobile", true);});
-// });
+d3.selectAll(".logo").on("click", function () {
+  var activeClass = "logoMobile";
+  var alreadyIsActive = d3.select(this).classed(activeClass);
+  d3.selectAll(".logo")
+  .classed(activeClass, false);
+  d3.select(this).classed(activeClass, !alreadyIsActive);
+});
